dcf4f91d1bcbced1c1d55558af675ac9b40b15ec
Dmytro Virych <damienissa@yahoo.com>
2021-12-15 11:04:15 +0200

    Initial Commit

---
+++ CoffeIT-app/CoffeIT_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeIT_appApp.swift
+//  CoffeIT-app
+//
+//  Created by Dmitry Virych on 15.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoffeIT_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
